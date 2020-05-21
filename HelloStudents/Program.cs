84a6e8915f1cbbcd168e5b9efe5f1c138a0e0bb5
Gihun Ham <progh2@gmail.com>
2020-05-21 10:22:46 +0900

    초기 커밋.

---
+++ HelloStudents/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloStudents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<StudentBase> students = new List<StudentBase>();
+
+            /*
+             *  1. Student3099Ham.cs 같은 클래스를 생성 후 Hello 메서드를 자신의 스타일로 수정해보세요.
+             *  2. Program.cs의 Main 메서드에 자신의 학번 위치에 1.에서 생성한 클래스의 
+             *     객체를 생성해서 추가하는 코드를 넣으세요.
+             */
+            // 3099
+            // 3101
+            // 3102
+            // 3103
+            // 3104
+            // 3105
+            // 3106
+            // 3107
+            // 3108
+            // 3109
+            // 3110
+            // 3111
+            // 3112
+            // 3113
+            // 3114
+            // 3115
+            // 3117
+            // 3118
+            // 3119
+            // 3120
+
+            // 3201
+            // 3202
+            // 3203
+            // 3204
+            // 3205
+            // 3206
+            // 3207
+            // 3208
+            // 3209
+            // 3210
+            // 3211
+            // 3212
+            // 3213
+            // 3214
+            // 3215
+            // 3217
+            // 3218
+            // 3219
+            // 3220
+
+            // 학번 순으로 정렬
+            students.Sort();
+
+            // 명단 출력
+            foreach( StudentBase student in students){
+                Console.WriteLine(student.Hello());
+            }
+        }
+    }
+}
---
+++ HelloStudents/StudentBase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloStudents
+{
+    abstract class StudentBase : IEquatable<StudentBase>, IComparable<StudentBase>
+    {
+        // 속성들 (멤버변수와 다름!)
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int StudentNumber { get; set; }
+
+        public int BaseYear { get; set; }
+
+        // 오버라이드한 메서드
+        public override string ToString()
+        {
+            return StudentNumber + " " + LastName +" " + FirstName;
+        }
+
+        // 구현해야 할 추상 메서드
+        public abstract string Hello();
+
+        // 같은지 비교하기 전에 비교 가능한 클래스인지 확인 후 같은지 비교하는 메서드
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            StudentBase objAsStudentBase = obj as StudentBase;
+            if (objAsStudentBase == null) return false;
+            else return Equals(objAsStudentBase);
+        }
+
+        // 비교를 위한 "같다" 재정의 메서드
+        public bool Equals(StudentBase other)
+        {
+            if (other == null) return false;
+            return (this.StudentNumber == other.StudentNumber);
+        }
+
+        // 정렬을 위한 비교 메서드
+        public int CompareTo(StudentBase compareStudentBase)
+        {
+            // A null value means that this object is greater.
+            if (compareStudentBase == null)
+                return 1;
+
+            else
+                return this.StudentNumber.CompareTo(compareStudentBase.StudentNumber);
+        }
+
+        // 해시코드 재정의 - 학번 기준으로. 즉 같은 학번 학생은 있을 수 없다.
+        public override int GetHashCode()
+        {
+            return BaseYear * 10000 + StudentNumber;
+        }
+
+
+    }
+}
