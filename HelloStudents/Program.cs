0500e012954bcc155a3a8e5ec41dd7008b5b885e
hyeonseo25 <s2019s30@e-mirim.hs.kr>
2021-03-29 15:55:39 +0900

    3110 Ryu 학생 정보 추가

--- HelloStudents/Program.cs
+++ HelloStudents/Program.cs
@@ -8,7 +8,7 @@ namespace HelloStudents
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // string 이 소문자
         {
             List<StudentBase> students = new List<StudentBase>();
 
@@ -34,6 +34,14 @@ namespace HelloStudents
             // 3108
             // 3109
             // 3110
+            students.Add(
+                new Student3110Ryu()
+                {
+                    FirstName = "현서",
+                    LastName = "류",
+                    StudentNumber = 3110,
+                    BaseYear = 2021
+                });
             // 3111
             // 3112
             // 3113
---
+++ HelloStudents/Student3110Ryu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloStudents
+{
+
+    class Student3110Ryu : StudentBase
+    {
+        public override string Hello()
+        {
+            return "안녕 나는 "+this+"!";
+        }
+    }
+}
