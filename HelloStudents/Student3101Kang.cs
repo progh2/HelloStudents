d498224e8d0214c67496968b565ce556af2ff78f
suyeon <rkdtndus9090@naver.com>
2021-03-29 15:52:31 +0900

    3101Kang 학생 정보 추가

---
+++ HelloStudents/Student3101Kang.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloStudents
+{
+    class Student3101Kang : StudentBase
+    {
+        public override string Hello()
+        {
+            return "안녕? 나는 " + this.ToString() + "님이셔라!";
+        }
+    }
+}
