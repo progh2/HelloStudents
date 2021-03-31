5ad8ccaf3af710abd4a7bd4cdc2a672c224cf1a2
Gihun Ham <ham@e-mirim.hs.kr>
2021-03-31 09:02:56 +0900

    Student3116Jo 통합 버그 해결 누락된 Student3116JO 프로젝트 등록 및 Students3116Jo -> Student3116Jo ; 누락 추가

--- HelloStudents/Student3116JO.cs
+++ HelloStudents/Student3116JO.cs
@@ -1,10 +1,10 @@
 ﻿namespace HelloStudents
 {
-    internal class Students3116JO : StudentBase
+    internal class Student3116JO : StudentBase
     {
         public override string Hello()
         {
-            return "안녕? 나는 " + this + "야."
+            return "안녕? 나는 " + this + "야.";
         }
     }
 }
