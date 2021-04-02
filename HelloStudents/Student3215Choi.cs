eb594cda4ef904d698373ae50963e603f4d49751
Minseo Choi <minseochoi0308@gmail.com>
2021-04-02 16:02:31 +0900

    3215최민서클래스 추가

---
+++ HelloStudents/Student3215Choi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloStudents
+{
+    class Student3215Choi : StudentBase
+    {
+        public override string Hello()
+        {
+            return this + "는 지금 아주 졸린 상태 입니다.";
+        }
+    }
+}
