465630f561232014df486472ebd21199eedb47d3
Gihun Ham <ham@e-mirim.hs.kr>
2021-04-02 16:02:17 +0900

    3299 함기훈 클래스 추가

--- HelloStudents/Program.cs
+++ HelloStudents/Program.cs
@@ -43,7 +43,7 @@ namespace HelloStudents
                    FirstName = "나은",
                    LastName = "김",
                    StudentNumber = 3103,
-                   BaseYear = 2020
+                   BaseYear = 2021
                });
             // 3104
             // 3105     
@@ -104,6 +104,15 @@ namespace HelloStudents
             // 3218
             // 3219
             // 3220
+            // 3299
+            students.Add(
+                new Student3299Ham()
+                {
+                    FirstName = "기훈",
+                    LastName = "함",
+                    StudentNumber = 3299,
+                    BaseYear = 2021
+                });
 
             // 학번 순으로 정렬
             students.Sort();
